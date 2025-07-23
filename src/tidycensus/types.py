c9ea9d49e28c0deac18ae2b562b051a9128c3443
Alistair Pattison <alistairpattison@gmail.com>
2025-07-22 22:38:32 -0400

    formatting

--- src/tidycensus/types.py
+++ src/tidycensus/types.py
@@ -1,14 +1,10 @@
-
-
-# TODO: add more surveys
 from typing import Literal
 
 
+# TODO: add more surveys
 Dataset = Literal["acs/acs5", "dec/sf3", "geoinfo"] | str
 
 # TODO: add more geographies
-Geography = Literal[
-    "us", "region", "division", "state", "county", "block group"
-]
+Geography = Literal["us", "region", "division", "state", "county", "block group"]
 
 AcsVersion = Literal["acs1", "acs3", "acs5"]
